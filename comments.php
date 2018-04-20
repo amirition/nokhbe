8f400965029291e77fd403a6cce20c32d295858d
Amirition <pahnaei@live.com>
2018-04-20 13:36:09 +0430

    Single page and comments

--- comments.php
+++ comments.php
@@ -1,1 +1,109 @@
 <?php
+function nk_comment_template($comment, $args, $depth) {
+    ?>
+    <div class="comment card cell medium-12 grid-x grid-margin-x">
+        <div class="cell medium-2">
+            <img src="<?= get_avatar_url(get_the_author_meta('ID')); ?>">
+        </div>
+        <div class="cell medium-10 grid-x">
+            <div class="comment-header cell medium-12 grid-x">
+                <h6 class="cell medium-3 text-right"><?php comment_author(); ?></h6>
+                <p class="cell medium-5 medium-offset-4 text-left"> <?= get_comment_date() . ' - ' . get_comment_time() ?></p>
+            </div>
+            <p class="cell medium-12"><?php comment_text($comment); ?></p>
+            <div class="comment-footer cell medium-12 grid-x">
+                <div class="cell medium-10">
+                    <?php
+                    if ( $comment->comment_approved == '0' ) { ?>
+                    <em class="comment-awaiting-moderation"><?php __( 'دیدگاه شما در انتظار بررسی است.', 'nokhbe' ); ?></em><?php
+                    } ?>
+                </div>
+                <div class="cell medium-2 reply text-left">
+	                <?php
+	                comment_reply_link(
+		                array_merge(
+			                $args,
+			                array(
+				                'depth'     => $depth,
+				                'max_depth' => $args['max_depth']
+			                )
+		                )
+	                ); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+}
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<div id="comments" class="comments-area cell medium-12 grid-x">
+
+	<?php
+	$comments_arg = ( array(
+		'fields'               => array(
+			'author' => '<input class="cell medium-6" type="text" name="author" placeholder="نام شما" required>',
+			'email'  => '<input class="cell medium-6" type="email" name="email" placeholder="ایمیل شما" required>',
+			'url'    => ' '
+		),
+		'comment_field'        => '<textarea placeholder="دیدگاه خود را بنویسید ..." name="comment" class="cell medium-12" id="comment" rows="5" required> </textarea>',
+		'class_form'           => 'comment-form grid-x grid-margin-x',
+		'class_submit'         => 'button primary cell medium-4',
+		'comment_notes_before' => __( '<p>تمامی فیلدها موردنیاز هستند، ایمیل شما منتشر نخواهد شد.</p>', 'nokhbe' ),
+		'title_reply'          => __( 'دیدگاهتان را بنویسید', 'nokhbe' ),
+		'title_reply_before'   => '<h5 class="cell medium-12">',
+		'title_reply_after'    => '</h5>'
+	) );
+	comment_form( $comments_arg );
+	if ( have_comments() ) : ?>
+        <h5 class="comments-title">
+			<?php
+			$comments_number = get_comments_number();
+			if ( '1' === $comments_number ) {
+				/* translators: %s: post title */
+				printf( _x( 'یک دیدگاه برای  &ldquo;%s&rdquo;', 'comments title', 'nokhbe' ), get_the_title() );
+			} else {
+				printf(
+				/* translators: 1: number of comments, 2: post title */
+					_nx(
+						'%1$s دیدگاه برای &ldquo;%2$s&rdquo;',
+						'%1$s دیدگاه برای &ldquo;%2$s&rdquo;',
+						$comments_number,
+						'comments title',
+						'nokhbe'
+					),
+					number_format_i18n( $comments_number ),
+					get_the_title()
+				);
+			}
+			?>
+        </h5>
+
+		<?php
+		wp_list_comments( array(
+			'avatar_size'   =>  100,
+			'style'         =>  'ul',
+			'short_ping'    =>  true,
+			'reply_text'    =>  __( 'پاسخ', 'nokhbe' ),
+            'max_depth'     =>  5,
+            'per_page'      =>  10,
+            'callback'      =>  'nk_comment_template'
+		) );
+		?>
+
+		<?php the_comments_pagination( array(
+			'prev_text' => '<span class="screen-reader-text">' . __( 'قبلی', 'nokhbe' ) . '</span>',
+			'next_text' => '<span class="screen-reader-text">' . __( 'بعدی', 'nokhbe' ) . '</span>',
+		) );
+	endif; // Check for have_comments().
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+        <p class="no-comments"><?php _e( 'دیدگاه ها بسته شده اند.', 'nokhbe' ); ?></p>
+		<?php
+	endif;
+	?>
+
+</div><!-- #comments -->
--- single.php
+++ single.php
@@ -1,1 +1,54 @@
 <?php
+get_header();
+$categories = get_the_category();
+?>
+	<div class="grid-x cell medium-10 medium-offset-1 small-12 main-content">
+		<aside class="cell medium-3 sidebar sidebar-right grid-y">
+			<?php
+			get_template_part( 'template-parts/sidebar-right' );
+			get_sidebar( 'right_sidebar' );
+			?>
+		</aside>
+		<div class="cell medium-6 single-post grid-x">
+			<h2><?= the_title(); ?></h2>
+			<div class="single-meta grid-x cell medium-12">
+				<div class="cell medium-4"><i class="fa fa-user"></i> <?php the_author() ?></div>
+				<div class="cell medium-4"><i class="fa fa-calendar"></i> <?= get_the_date() ?></div>
+				<div class="telegram-share cell medium-4">
+					<a title="اشتراک در تلگرام" href="https://telegram.me/share/url?url=<?php the_permalink(); ?>" target="_blank"><i class="fa fa-telegram"></i>  اشتراک گذاری در تلگرام </a>
+				</div>
+			</div>
+			<div class="cell medium-12">
+				<img src="<?= get_the_post_thumbnail_url() ?>">
+			</div>
+			<p><?php the_content(); ?></p>
+			<hr>
+			<div class="tags cell medium-12">
+				<i class="fa fa-tags"></i> <?php the_tags('برچسب ها: ', ' ') ?>
+			</div>
+			<div class="categories cell medium-12">
+				<i class="fa fa-list-ul"></i>
+				<?php
+				foreach ($categories as $category) {
+					?>
+					دسته بندی ها: <a href="<?= get_category_link($category->term_id)?>"> <?= $category->name ?> </a>
+					<?php
+				}
+				?>
+			</div>
+			<hr>
+			<?php
+			if ( comments_open() ) :
+				comments_template();
+			endif;
+			?>
+		</div>
+		<aside class="cell medium-3 sidebar sidebar-left grid-y">
+			<?php
+			get_template_part('template-parts/sidebar-left');
+			get_template_part('template-parts/sidebar-ads');
+			?>
+		</aside>
+	</div>
+<?php
+get_footer();
